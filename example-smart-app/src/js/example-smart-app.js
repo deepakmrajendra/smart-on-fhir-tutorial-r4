c1deae901dabeae18d0651c92b2b1f82b660a9d8
deepakmrajendra <deepakm.rajendra@gmail.com>
2025-05-31 13:33:11 +0100

    Changes to modernize code to use FHIR R4 instead of FHIR DSTU2

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,139 +1,105 @@
-(function(window){
-  window.extractData = function() {
-    var ret = $.Deferred();
+(function(window) {
+  window.extractData = async function() {
+    try {
+      // Get the client
+      const client = await FHIR.oauth2.ready();
+      
+      if (client.patient) {
+        // Get patient resource
+        const patient = await client.patient.read();
+        
+        // Get observations using R4 syntax
+        const queryParams = new URLSearchParams();
+        queryParams.set('patient', client.patient.id);
+        queryParams.set('code', [
+          'http://loinc.org|8302-2',   // Height
+          'http://loinc.org|8462-4',   // Diastolic BP
+          'http://loinc.org|8480-6',   // Systolic BP
+          'http://loinc.org|2085-9',   // HDL
+          'http://loinc.org|2089-1',   // LDL
+          'http://loinc.org|55284-4',  // BP
+          'http://loinc.org|29463-7',  // Weight
+          'http://loinc.org|39156-5',  // BMI
+          'http://loinc.org|8310-5',   // Temperature
+          'http://loinc.org|8867-4',   // Heart rate
+          'http://loinc.org|2339-0'    // Glucose
+        ].join(','));
+        
+        const observations = await client.request(`Observation?${queryParams}`);
 
-    function onError() {
-      console.log('Loading error', arguments);
-      ret.reject();
-    }
+        // Helper function to find observations by code
+        const byCodes = (code) => {
+          if (!observations.entry) return [];
+          return observations.entry
+            .filter(entry => entry.resource.code.coding
+              .some(coding => coding.code === code))
+            .map(entry => entry.resource);
+        };
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
-                              'http://loinc.org|29463-7', 'http://loinc.org|39156-5',
-                              'http://loinc.org|8310-5',  // Temperature
-                              'http://loinc.org|8867-4',  // Heart rate
-                              'http://loinc.org|2339-0'   // Glucose
-                             ]
-                      }
-                    }
-                  });
+        // Create the patient object with modern JS
+        const p = {
+          // Demographics
+          fname: patient.name?.[0]?.given?.join(' ') || '',
+          lname: patient.name?.[0]?.family || '',
+          gender: patient.gender || '',
+          birthdate: patient.birthDate || '',
+          
+          // Observations
+          height: getQuantityValueAndUnit(byCodes('8302-2')[0]),
+          weight: getQuantityValueAndUnit(byCodes('29463-7')[0]),
+          bmi: getQuantityValueAndUnit(byCodes('39156-5')[0]),
+          temperature: getQuantityValueAndUnit(byCodes('8310-5')[0]),
+          heartrate: getQuantityValueAndUnit(byCodes('8867-4')[0]),
+          glucose: getQuantityValueAndUnit(byCodes('2339-0')[0]),
+          
+          // Blood pressure requires special handling
+          ...getBloodPressureValues(byCodes('55284-4')),
+          
+          // Cholesterol
+          hdl: getQuantityValueAndUnit(byCodes('2085-9')[0]),
+          ldl: getQuantityValueAndUnit(byCodes('2089-1')[0])
+        };
 
-        $.when(pt, obv).fail(onError);
-
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-
-          var fname = '';
-          var lname = '';
-
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
-
-          var height = byCodes('8302-2');
-          var weight = byCodes('29463-7');
-          var bmi = byCodes('39156-5');
-          var temperature = byCodes('8310-5');
-          var heartrate = byCodes('8867-4');
-          var glucose = byCodes('2339-0');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
-
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-          p.weight = getQuantityValueAndUnit(weight[0]);
-          p.bmi = getQuantityValueAndUnit(bmi[0]);
-          p.temperature = getQuantityValueAndUnit(temperature[0]);
-          p.heartrate = getQuantityValueAndUnit(heartrate[0]);
-          p.glucose = getQuantityValueAndUnit(glucose[0]);
-
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
-
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-
-          ret.resolve(p);
-        });
+        return p;
       } else {
-        onError();
+        throw new Error('Patient context not found');
       }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
     }
-
-    FHIR.oauth2.ready(onReady, onError);
-    return ret.promise();
-
   };
 
-  function defaultPatient(){
+  // Helper function to get both systolic and diastolic BP
+  function getBloodPressureValues(BPObservations) {
+    if (!BPObservations || !BPObservations[0]) return { systolicbp: '', diastolicbp: '' };
+    
+    const observation = BPObservations[0];
+    const systolic = observation.component?.find(component => 
+      component.code.coding.some(coding => coding.code === '8480-6')
+    );
+    const diastolic = observation.component?.find(component => 
+      component.code.coding.some(coding => coding.code === '8462-4')
+    );
+
     return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      weight: {value: ''},
-      bmi: {value: ''},
-      temperature: {value: ''},
-      heartrate: {value: ''},
-      glucose: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
+      systolicbp: getQuantityValueAndUnit({ valueQuantity: systolic?.valueQuantity }) || '',
+      diastolicbp: getQuantityValueAndUnit({ valueQuantity: diastolic?.valueQuantity }) || ''
     };
   }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
-
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
+  // Helper function to get value and unit from an observation
+  function getQuantityValueAndUnit(observation) {
+    const valueQuantity = observation?.valueQuantity;
+    if (!valueQuantity) return '';
+    
+    const { value, unit } = valueQuantity;
+    if (typeof value !== 'number') return '';
+    
+    return `${value} ${unit || ''}`.trim();
   }
 
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
-  }
-
+  // Visualization function remains the same since we're keeping jQuery
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
@@ -152,5 +118,4 @@
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
   };
-
 })(window);
